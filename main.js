a3560764137a8c9f44fd9d249e779d3a5f5c2d67
Suraj <surajverma8730448@gmail.com>
2023-09-01 15:55:19 +0530

    portfolio content added

---
+++ main.js
@@ -0,0 +1,16 @@
+// This is for sidebar
+
+const openBtn = document.querySelector('.open-btn');
+const closeBtn = document.querySelector('.close-btn');
+const offcanvasMenu = document.querySelector('.side-navbar');
+
+openBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    offcanvasMenu.classList.add('active');
+});
+
+closeBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    offcanvasMenu.classList.remove('active');
+});
+
